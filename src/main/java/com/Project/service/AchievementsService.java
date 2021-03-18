722340246a621b251d9b03ce7c538e9ba38eabe5
Hasan2510 <hasan251096@outlook.com>
2021-03-18 17:45:18 +0000

    TDL-14 read done

--- src/main/java/com/Project/service/AchievementsService.java
+++ src/main/java/com/Project/service/AchievementsService.java
@@ -1,1 +1,47 @@
+package com.Project.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Project.Exceptions.AchievementNotFoundException;
+import com.Project.Mappers.AchievementsMappers;
+import com.Project.data.model.Achievements;
+import com.Project.data.repository.AchievementsRepository;
+import com.Project.dto.AchievementDTO;
+
+@Service
+public class AchievementsService {
+
+	private AchievementsRepository achievementsRepository;
+	private AchievementsMappers achievementsMapper;
+
+	@Autowired
+	public AchievementsService(AchievementsRepository achievementsRepository, AchievementsMappers achievementsMapper) {
+		this.achievementsRepository = achievementsRepository;
+		this.achievementsMapper = achievementsMapper;
+	}
+
+	public List<AchievementDTO> readAllAchievements() {
+		List<Achievements> achievements = achievementsRepository.findAll();
+		List<AchievementDTO> achievementDTO = new ArrayList<>();
+		achievements.forEach(a -> achievementDTO.add(achievementsMapper.mapToDTO(a)));
+		return achievementDTO;
+
+	}
+
+	public AchievementDTO readById(Integer id) {
+		Optional<Achievements> achievement = achievementsRepository.findById(id);
+
+		if (achievement.isPresent()) {
+			return achievementsMapper.mapToDTO(achievement.get());
+		} else {
+			throw new AchievementNotFoundException("Achievement is not found");
+		}
+	}
+}
